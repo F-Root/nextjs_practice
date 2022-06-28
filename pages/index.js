06a301108a64b31a5e1d117ecd52a5ae9ee0f6b9
RooT_Kim <bdd3452@gmail.com>
2022-06-28 13:30:52 +0900

    Next JS Routing (use directory)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
-  return <div>hello, nextjs 3 - exp</div>;
+  return (
+    <div>
+      <h1>hello, nextjs 3 - exp</h1>
+      <ol>
+        <li>
+          <Link href='/sub'>/sub/index.js</Link>
+        </li>
+        <li>
+          <Link href='/sub/about'>/sub/about.js</Link>
+        </li>
+        <li>
+          <Link href='/sub/1'>/sub/[id].js</Link>
+        </li>
+      </ol>
+    </div>
+  );
 }
